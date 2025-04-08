c742333a347d1da53b8e9f8427645d2b23181ed6
FuzzyExpress <127012288+FuzzyExpress@users.noreply.github.com>
2025-04-07 22:37:31 -0500

    Add turret graphics, improve render script

--- art/render.py
+++ art/render.py
@@ -1,5 +1,6 @@
 import bpy
 import os
+import math
 
 bpy.data.scenes["Scene"].render.film_transparent = True
 
@@ -27,19 +28,105 @@ def render(name, heading):
 # split by space for easy making
 headings = 'd S E N NS WE W SW NW NE SE NESW'.split()
 
-bpy.data.objects["Camera"].location[1] = 0.5
 
-bpy.data.scenes["Scene"].render.resolution_x = 40
-bpy.data.scenes["Scene"].render.resolution_y = 40
+def size(s):
+    bpy.data.scenes["Scene"].render.resolution_x = s
+    bpy.data.scenes["Scene"].render.resolution_y = s
 
-for x in range(12):
-    bpy.data.objects["Path"].modifiers["GeometryNodes"]["Socket_2"] = x
-    render( "path", headings[x] )
+def move(x, y):
+    bpy.data.objects["Camera"].location[0] = x + .5
+    bpy.data.objects["Camera"].location[1] = y + .5
 
-bpy.data.objects["Camera"].location[1] = 4.5
+def scale(s):
+    size( round(s*40) )
+    bpy.data.cameras["Camera"].ortho_scale = s
 
-for x in range(12):
-    bpy.data.objects["Grass"].modifiers["GeometryNodes"]["Socket_2"] = x
-    render( "grass", headings[x] )
+def ToggleRender(col, shot):
+    for each in bpy.data.collections.get(col).objects:
+        if "bool" in each.name: continue
+        each.hide_render = True if "flash" in each.name else False
+        if shot: each.hide_render = not each.hide_render
+
+
+move(0, 2); scale(1);
+
+# render( "unknown", "image" )
+
+# move(0, 0)
+
+# for x in range(12):
+#     bpy.data.objects["Path"].modifiers["GeometryNodes"]["Socket_2"] = x
+#     render( "path", headings[x] )
+
+# move(0, 4)
+
+# for x in range(12):
+#     bpy.data.objects["Grass"].modifiers["GeometryNodes"]["Socket_2"] = x
+#     render( "grass", headings[x] )
+
+move(2, 2); scale(1.5);
+
+# simple pew pew
+for degree in range(0, 360, 20):
+    ToggleRender("Simple Turret", False)
+    
+    for rot in [0, -19/3 -19/3*2]:
+        bpy.data.objects["Plane.003"].rotation_euler[2] = math.radians(degree)
+        bpy.data.objects["Plane.008"].rotation_euler[1] = math.radians(rot)
+        render( "SimpleTurret", f"{degree}-{rot}" )
+
+    ToggleRender("Simple Turret", True)
+    render( "SimpleTurret", f"{degree}-flash" )
+
+    
+move(4, 2); scale(2);
+
+# chain pew pew
+for degree in range(0, 360, 20):
+    ToggleRender("Chain Turret", False)
+    
+    for rot in [0, -46.3/3 -46.3/3*2]:
+        bpy.data.objects["Plane.011"].rotation_euler[2] = math.radians(degree)
+        bpy.data.objects["Plane.005"].rotation_euler[1] = math.radians(rot)
+        render( "ChainTurret", f"{degree}-{rot}" )
+
+    ToggleRender("Chain Turret", True)
+    render( "ChainTurret", f"{degree}-flash" )
+
+    
+move(6, 2); scale(2.5);
+
+# sniper
+for degree in range(0, 360, 20):
+    ToggleRender("Sniper Turret", False)
+    
+    for recoil in [0]:  # add later maybe
+        bpy.data.objects["Plane.013"].rotation_euler[2] = math.radians(degree)
+        render( "SniperTurret", f"{degree}-0" )
+
+    ToggleRender("Sniper Turret", True)
+    render( "SniperTurret", f"{degree}-flash" )
+
+    
+move(8, 2); scale(1);
+
+# sniper
+for degree in range(0, 360, 20):
+    ToggleRender("Bonk Turret", False)
+    
+    for recoil in [0]:  # add later maybe
+        bpy.data.objects["Circle.003"].rotation_euler[2] = math.radians(degree)
+        render( "BonkTurret", f"{degree}-0" )
+
+        
+
+
+
+
+
+
 
 bpy.data.scenes["Scene"].render.film_transparent = False
+
+
+
